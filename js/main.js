effa4a3b3436696dff7e424736a5c384efe4367a
Artem Arshaniy <artem.arshaniy108@gmail.com>
2016-12-15 12:56:29 +0200

    add delete btn

--- js/main.js
+++ js/main.js
@@ -53,19 +53,23 @@ init();
 function Controller(model, view) {
 var self = this;
 
-view.elements.addBtn.on('click',addItem);
-view.element.listContainer.on('click', 'item-delete', removeItem);
-console.log( addItem);
+view.elements.addBtn.on('click', addItem);
+view.elements.listContainer.on('click', 'item-delete', removeItem);
+
 
 function addItem() {
   var newItem = view.elements.input.val();
+  
   model.addItem(newItem);
   view.renderList(model.data);
   view.elements.input.val('');
 }
 
 function removeItem() {
+var item = $ (this).attr('data-value');
 
+model.removeItem (item);
+view.renderList(model.data);
 }
 
 }
